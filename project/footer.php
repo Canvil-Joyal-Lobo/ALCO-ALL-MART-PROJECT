991725112eff731b5e795fac7275e491288d2f87
Canvil Joyal Lobo <canvillobo1212@gmail.com>
2024-07-27 23:13:59 +0530

    Add files via upload

---
+++ project/footer.php
@@ -0,0 +1,36 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php">Home</a>
+         <a href="shop.php">Shop</a>
+         <a href="contact.php">Contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="login.php">Login</a>
+         <a href="register.php">Register</a>
+         <a href="cart.php">Cart</a>
+         <a href="orders.php">Orders</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <p> <i class="fas fa-phone"></i> +919019826618 </p>
+         <p> <i class="fas fa-envelope"></i> canvillobo1212@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> Mangalore, karnataka,India - 575015 </p>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="https://www.facebook.com/canviljoyal.lobo.7?mibextid=LQQJ4d"> <i class="fab fa-facebook-f"></i> Facebook </a>
+         <a href="#https://www.instagram.com/canviljoyallobo?igsh=MTQ1am00YmhheXpkZA=="> <i class="fab fa-instagram"></i> Instagram </a>
+      </div>
+
+   </div>
+
+
+</section>
